a7eb9e26531f41f3aaf7105c604cb6ee7ffa3b59
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-01-25 17:48:32 +0900

    Add basic help option

--- app-icon-resize-machine/main.swift
+++ app-icon-resize-machine/main.swift
@@ -6,6 +6,25 @@
 //
 
 import Foundation
+import ArgumentParser
 
-print("Hello, World!")
+struct ResizeMachine: ParsableCommand {
+    static var configuration: CommandConfiguration = CommandConfiguration(
+        commandName: "ResizeMachine",
+        abstract: "Resizes .png file and generates Appicon file",
+        discussion:
+            """
+ddddd
+""",
+        version: "0.1.0",
+        shouldDisplay: true,
+//        subcommands: ,
+//        defaultSubcommand: ,
+        helpNames: [.long, .short])
+    
+    func run() throws {
+        print("Hello, World!")
+    }
+}
 
+ResizeMachine.main()
